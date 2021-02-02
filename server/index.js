3cef19782e3d9416865671295f881280fd4707be
Artem <rozen19942810@gmail.com>
2021-02-02 17:58:50 +0200

    add app-skeleton

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+
+const start = () => {
+
+    try {
+
+    } catch (e) {
+
+    }
+}
